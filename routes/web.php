43b979df3676a031026f10fa454e7d28afab2172
afifmucholis <afif.mucholis@gmail.com>
2020-03-09 23:09:15 +0700

    Update web.php
    subdomain routing

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@
 Auth::routes();
 
 
-	// Route::get('/home', 'HomeController@index')->name('home');
-// Route::group(['domain' => '{subdomain}.address.test'], function () {
-//      Route::get('/', function($subdomain){
-//         if ($subdomain == 'dashboard') {
-//         	return redirect()->action('HomeController@index');
-//         }elseif ($subdomain == 'backoffice') {
-//         	return redirect()->action('AdminController@index');
-//         }
-//     })->name('sub'); 
-// });
+
+Route::group(['domain' => '{subdomain}.localhost'], function () {
+     Route::get('/', function($subdomain){
+        if ($subdomain == 'dashboard') {
+        	return redirect()->action('HomeController@index');
+        }elseif ($subdomain == 'backoffice') {
+        	return redirect()->action('AdminController@index');
+        }
+    })->name('sub'); 
+});
 
 // Route::group(['domain' => 'dashboard.address.test'], function () {
 //     Route::get('/', 'HomeController@index');
@@ -37,9 +37,9 @@ Auth::routes();
 //     });
 // });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', 'HomeController@index');
 // Route::get('/backoffice', 'AdminController@index');
